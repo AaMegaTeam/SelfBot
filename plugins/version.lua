f043a5dd5b286155ee30823624b95b366b228185
AaMegaTeam <hamed.dehghani321@gmail.com>
2016-10-11 03:40:14 -0700

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,7 +1,7 @@
 do
 
 local function run(msg, matches)
-      local text = "Beyond Self Bot V2.5\nAn Fun Bot Based On TeleSeed Written In Lua\n\nSudo User :\nDeveloper&Founder : @SoLiD021\nDeVeloper&Manager : @CliApi\n\nTeam Channel :\n@BeyondTeam\n\nSpecial Thx To :\nSeed Team\nAnd All My Friends :D\n\nBeyond Self Bot Version 2.5 On GitHub :\nGithub.com/BeyondTeam/Self-Bot"
+      local text = "<b>Yaghi Self Bot</b> V2.5\nAn Fun Bot Based On TeleSeed Written In Lua\n\nSudo User :\nDeveloper&Founder : @teshne\nDeVeloper&Manager : @Horizon_rz\n\nTeam Channel :\n@pedaret\n\nSpecial Thx To :\nSeed Team\nAnd \n BeyondTeam\nAnd All My Friends :D"
   if matches[1]:lower() == 'beyondself' or 'version' or 'ver' or 'git' then --change this with anything you want
 reply_msg(msg.id, text, ok_cb, false)
   end
@@ -9,8 +9,8 @@ end
 
 return {
   patterns = {
-    "^[!/#]([Bb]eyondself)$",
-    "^([Bb]eyondself)$",
+    "^[!/#]([Yy]aghiself)$",
+    "^([Yy]aghiself)$",
     "^[!/#]([Vv]ersion)$",
     "^([Vv]ersion)$",
     "^([Gg]it)$",
